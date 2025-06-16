061bb9f9de5b1699ff9e6906f6b5f8bb8a23657b
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-16 16:47:53 +0200

    Création de la structure ! :)

---
+++ src/exercicesAnswers.js
@@ -0,0 +1,85 @@
+
+//Exercice 1 : Donner la taille de la chaîne de caractères.
+let stringLength = function (sentence) {
+  // ici votre return
+}
+
+//Exercice 2 : Remplacer le premier "e" de la chaîne par un espace.
+let replaceFirstEBySpace = function (sentence) {
+  // ici votre return
+}
+
+//Exercice 3 : Concaténer les deux chaînes de caractères.
+let concatenateSentences = function (firstSentence, secondSentence) {
+  // ici votre return
+}
+
+//Exercice 4 : Afficher le cinquième caractère de la chaîne.
+let displayFifthCharacter = function (sentence) {
+  // ici votre return
+}
+
+//Exercice 5 : Afficher les 9 premiers caractère.
+let displayFirstNineCharacters = function (sentence) {
+  // ici votre return
+}
+
+//Exercice 6 : Mettre en majuscule la chaîne.
+let upperTheSentence = function (sentence) {
+  // ici votre return
+}
+
+//Exercice 7 : Mettre en minuscule la chaîne.
+let lowerTheSentence = function (sentence) {
+  // ici votre return
+}
+
+//Exercice 8 : Supprimer les espaces avant et après la chaîne.
+let removeSpaces = function (sentence) {
+  // ici votre return
+}
+
+//Exercice 9 : Afficher true si le paramètre d'entrée de la fonction est de type *string*.
+let isString = function (sentence) {
+  // ici votre return
+}
+
+//Exercice 10 : Afficher l'extension du fichier.
+let displayExtension = function (fileName) {
+  // ici votre return
+}
+
+//Exercice 11 : Compter le nombre d'espace dans la chaîne.
+let countSpaces = function (sentence) {
+  // ici votre return
+}
+
+//Exercice 12 : Inverser une chaîne de caractères.
+let invertSentence = function (sentence) {
+  // ici votre return
+}
+
+//Exercice 13 : Chercher si dans la chaîne de caractère se trouve "Afpa".
+let searchInSentence = function (sentence) {
+  // ici votre return
+}
+
+//Exercice 14 : Afficher la valeur absolue d'un nombre.
+let displayAbsoluteValue = function (number) {
+  // ici votre return
+}
+
+//Exercice 15 : Afficher les valeurs absolues de plusieurs nombres.
+let displayAbsoluteValues = function (numbersArray) {
+  // ici votre return
+}
+
+//Exercice 16 : Calculer la surface d'un cercle en fonction de son rayon. L'arrondir à l'entier le plus proche.
+let calculateArea = function (radius) {
+  // ici votre return
+}
+
+//Exercice 17 : Calculer l'hypothènuse d\'un triangle rectangle. Arrondir à l'entier inférieur.
+let calculateHypotenuse = function (a, b) {
+  // ici votre return
+}
